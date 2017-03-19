62a6e82365fd42d34b8e5f4d93c4238199e63ec5
Cedric Ith <cedric.ith1@gmail.com>
2017-03-19 11:58:21 -0700

    Adding results page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 "use strict";
 
 (function(){
+	var exposureScore = -40;
+	var credibilityScore = 79;
+
 	window.onload = function() {
-		
+		calculateInput();
+		renderExposureScore();
+		renderCredibilityScore();
+	}
+
+	function calculateInput() {
+
+	}
+
+	function renderExposureScore() {
+		$("#finalscore").text() = exposureScore;
+	}
+
+	function renderCredibilityScore() {
+		$("#credibility").text() = credibilityScore;
 	}
 })();
